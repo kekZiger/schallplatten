529984f02db5aaf1866c1aeb18cdd8eff549cf1d
kekZiger <kekz.trust@gmail.com>
2023-12-30 18:28:51 +0100

    Erst einmal das Tutorial durcharbeiten.. Das letzte Mal Codeigniter war bei V3.. :D

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -7,7 +7,12 @@ use CodeIgniter\Router\RouteCollection;
  */
 $routes->get('/', 'Home::index');
 
+use App\Controllers\News;
 use App\Controllers\Pages;
+
+$routes->get('news', [News::class, 'index']);           // Add this line
+$routes->get('news/(:segment)', [News::class, 'show']); // Add this line
+
 
 $routes->get('pages', [Pages::class, 'index']);
 $routes->get('pages/(:segment)', [Pages::class, 'view']);
